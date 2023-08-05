079dbd922066ea24df7e0470d0dd367056668d24
syahridho <syahridhosyahputra@gmail.com>
2023-08-05 17:22:02 +0700

    1

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "16px",
+      },
+      colors: {
+        primary: "#db2777",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
